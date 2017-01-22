cb442d1ffb3ceede0549047d620ed2903c4d591a
Roman <iromanvm@gmail.com>
2017-01-22 15:27:51 +0200

    Initial commit

---
+++ src/main/java/controllers/MirrorServlet.java
@@ -0,0 +1,27 @@
+package controllers;
+
+import view.PageGenerator;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by oj on 22.01.2017.
+ */
+public class MirrorServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws ServletException, IOException{
+        Map<String, Object> gg = new HashMap<>();
+        String msn = request.getParameter("key");
+        gg.put("key", msn == null ? "" : msn);
+        response.getWriter().println(request.getParameter("key")/*PageGenerator.instance().getPage("mirror.html", gg)*/);
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+}
---
+++ src/main/java/main/Main.java
@@ -0,0 +1,25 @@
+package main;
+
+import controllers.MirrorServlet;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+/**
+ * Created by oj on 22.01.2017.
+ */
+public class Main {
+    public static void main(String[] args) throws Exception {
+        MirrorServlet mirrorServlet = new MirrorServlet();
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(mirrorServlet), "/*");
+
+        Server server = new Server(8080);
+        server.setHandler(context);
+
+        server.start();
+        System.out.println("Server started");
+        server.join();
+    }
+}
